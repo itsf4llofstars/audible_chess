1743074a116f7bef1a1541d509e166ad7cedc8d9
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-07 14:09:04 -0500

    Correct ["regex"] to regex["max_move"]

--- src/pgn_parsers.py
+++ src/pgn_parsers.py
@@ -10,14 +10,24 @@ def get_game_length(move_min: str, move_max: str, games) -> object:
     regex["max_move"] = r"\s" + move_max + r"\.\s"
 
     for game in games:
-        if re.search(regex["min_move"], game) and not re.search(["regex"], game):
+        if re.search(regex["min_move"], game) and not re.search(
+            regex["max_move"], game
+        ):
             length_games.append(game)
 
     return length_games
 
 
 def main():
-    ...
+    game_list = [
+        "1. xx xx 2. xx xx 10. NO xx 11. xx xx 40. xx xx 41. xx xx",
+        "1. xx xx 2. xx xx 10. YE xx 11. xx xx 38. xx xx 39. xx xx",
+        "1. xx xx 2. xx xx 10. NO xx 11. xx xx 40. xx xx 41. xx xx",
+        "1. xx xx 2. xx xx 10. YE xx 11. xx xx 38. xx xx 39. xx xx",
+    ]
+
+    chess_games = get_game_length("10", "40", game_list)
+    [print(game) for game in chess_games]
 
 
 if __name__ == "__main__":
