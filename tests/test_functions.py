6512a251ed44298e0a4e101231ea4982e67f7bd6
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-07 01:33:22 -0500

    Write TestFunctions class and test regex read pgn

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,5 +1,18 @@
 """test_functions.py"""
 import unittest
+from src.functions import regex_pgn_file
+
+
+class TestFuncitions(unittest.TestCase):
+    """Tests for the functions.py file"""
+
+    def test_regex_pgn_file(self):
+        """Test to show function returns proper beginning
+        and end of the chess games. This uses the unittest.pgn
+        file
+        """
+        test_games = regex_pgn_file("~/python/audible_chess_docs/unittest.pgn")
+        self.assertEqual(["1. xx xx 2. xx xx 1-0", "1. xx xx 2. xx 0-1"])
 
 
 if __name__ == "__main__":
