2d02f82f5c804f1ed75b5db91fb36f3034475234
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-06 20:10:36 -0500

    Cleaned up linting errors
    Write Docstring, filename.open() call, re-
    instantiate the chess_games list

--- src/functions.py
+++ src/functions.py
@@ -3,33 +3,42 @@ import re
 
 
 def regex_pgn_file(filename: str) -> object:
-    """Read a chess pgn file and return
-    those lines beginning with "^(1.\s)" and end with
-    "(\s[1-0|0-1])$" as a list of str's
+    r"""Read a chess pgn file.
 
-    Requires import re
+    Return those lines beginning with "^(1.\s)"
+    and end with "(\s[1-0|0-1])$" as a list of str's
+
+    Args:
+        filename (str): Path and name of the pgn file
+
+    Returns:
+        List(str): Only strings of chess games
     """
     game_begin = re.compile(r"^(1\.\s1\s)")
     game_end = re.compile(r"(\s[1-0|0-1])$")
-    chess_games = list()
+    chess_games = []
 
     try:
-        with open(filename) as read:
+        with filename.open() as read:
             for line in read:
                 if re.seaerch(game_begin, line) and re.search(game_end, line.rtrip()):
                     chess_games.append(line.rstrip())
     except FileNotFoundError as fnfe:
         # Log this
         print(f"{fnfe}")
-    finally:
-        if chess_games:
-            return chess_games
-        return
+
+    if chess_games:
+        return chess_games
 
 
-def main():
-    ...
+def main() -> None:
+    """main function"""
+    pgn_name = os.path.expanduser(
+        os.path.join("~", "python", "audible_chess", "docs", "tests.pgn")
+    )
 
 
 if __name__ == "__main__":
+    import os
+
     main()
