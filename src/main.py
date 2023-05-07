37aaf68cf1cf87fd09db79c750f1e945a619187f
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-06 19:43:28 -0500

    Add header and shebang

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
+"""main.py
 
+Audible Chess Project main Python file
+version 0.0.0
+
+Licensed under the:
+GNU GENERAL PUBLIC LICENSE
+Version 3, 29 June 2007
+
+itsf4llofstars
+irooted4hal@mailfence.com
+2023
+"""
