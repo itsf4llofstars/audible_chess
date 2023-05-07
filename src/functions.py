c734c517eaa0b75cc49b84e9db1abd777b094c3e
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-06 19:59:22 -0500

    Add regex_pgn_file function
    Functions reads a pgn file and returns a list of
    chess games that begin with 1.  and end with 1-0
    or 0-1

--- src/functions.py
+++ src/functions.py
@@ -1,9 +1,35 @@
 """functions.py"""
+import re
+
+
+def regex_pgn_file(filename: str) -> object:
+    """Read a chess pgn file and return
+    those lines beginning with "^(1.\s)" and end with
+    "(\s[1-0|0-1])$" as a list of str's
+
+    Requires import re
+    """
+    game_begin = re.compile(r"^(1\.\s1\s)")
+    game_end = re.compile(r"(\s[1-0|0-1])$")
+    chess_games = list()
+
+    try:
+        with open(filename) as read:
+            for line in read:
+                if re.seaerch(game_begin, line) and re.search(game_end, line.rtrip()):
+                    chess_games.append(line.rstrip())
+    except FileNotFoundError as fnfe:
+        # Log this
+        print(f"{fnfe}")
+    finally:
+        if chess_games:
+            return chess_games
+        return
 
 
 def main():
     ...
 
-    
+
 if __name__ == "__main__":
     main()
