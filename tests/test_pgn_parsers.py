950fd6a9bb19b0b4876933299a167e6809a200e3
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-07 18:37:39 -0500

    Add initial TestGamesList test class
    Add test_get_game-length method setup

--- tests/test_pgn_parsers.py
+++ tests/test_pgn_parsers.py
@@ -1,6 +1,17 @@
 """test_pgn_parsers.py"""
 import unittest
 
+from src.pgn_parsers import get_game_length
+
+
+class TestGamesList(unittest.TestCase):
+    """Unittests to test the pgn_parser.py file
+    functions
+    """
+
+    def test_get_game_length(" 10 ", " 40 ", game_lengths):
+        pass
+
 
 if __name__ == "__main__":
     unittest.main()
