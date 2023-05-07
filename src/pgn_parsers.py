ce3486f0dbd757b992d17d3b9b3de2a5ad3eebdd
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-07 13:56:36 -0500

    write initial game_length function

--- src/pgn_parsers.py
+++ src/pgn_parsers.py
@@ -1,7 +1,19 @@
 """pgn_parsers.py"""
 import re
 
-regex = {"min_move": r"\s10\.\s", "max_move": r"\s40.\s"}
+regex = {"min_move": r"", "max_move": r""}
+
+
+def get_game_length(move_min: str, move_max: str, games) -> object:
+    length_games = []
+    regex["min_move"] = r"\s" + move_min + r"\.\s"
+    regex["max_move"] = r"\s" + move_max + r"\.\s"
+
+    for game in games:
+        if re.search(regex["min_move"], game) and not re.search(["regex"], game):
+            length_games.append(game)
+
+    return length_games
 
 
 def main():
