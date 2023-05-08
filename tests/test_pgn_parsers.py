412b4c2fbec0017bcef15f0af0dda1ca51b7ec87
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-07 19:46:48 -0500

    Add second game to return in test

--- tests/test_pgn_parsers.py
+++ tests/test_pgn_parsers.py
@@ -7,6 +7,7 @@ game_lengths = [
     "1. xx 10. xx 40. xx",
     "1. xx 9. xx 39. xx",
     "1. xx 10. xx 39. xx",
+    "1. xx 2. xx 10. x 25.",
     "1. xx 9. xx 40. xx",
 ]
 
@@ -21,7 +22,7 @@ class TestGamesList(unittest.TestCase):
         max_move in them
         """
         test_chess_games = get_game_length("10", "40", game_lengths)
-        self.assertEqual(test_chess_games, ["1. xx 10. xx 39. xx"])
+        self.assertEqual(test_chess_games, ["1. xx 10. xx 39. xx", "1. xx 2. xx 10. x 25."])
 
 
 if __name__ == "__main__":
