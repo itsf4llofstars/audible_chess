53556d79fbc22dca8770f0ba8e3d4eb3a5a31ae2
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-07 19:42:49 -0500

    Write tst_get_game_length method

--- tests/test_pgn_parsers.py
+++ tests/test_pgn_parsers.py
@@ -3,14 +3,25 @@ import unittest
 
 from src.pgn_parsers import get_game_length
 
+game_lengths = [
+    "1. xx 10. xx 40. xx",
+    "1. xx 9. xx 39. xx",
+    "1. xx 10. xx 39. xx",
+    "1. xx 9. xx 40. xx",
+]
+
 
 class TestGamesList(unittest.TestCase):
     """Unittests to test the pgn_parser.py file
     functions
     """
 
-    def test_get_game_length(" 10 ", " 40 ", game_lengths):
-        pass
+    def test_get_game_length(self):
+        """Test if returned games has the min_move and not the
+        max_move in them
+        """
+        test_chess_games = get_game_length("10", "40", game_lengths)
+        self.assertEqual(test_chess_games, ["1. xx 10. xx 39. xx"])
 
 
 if __name__ == "__main__":
