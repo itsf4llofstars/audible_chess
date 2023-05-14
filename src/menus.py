8db6da3016059d36090c16042904a897798d7a8c
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-13 22:57:28 -0500

    Add get users_choice function

--- src/menus.py
+++ src/menus.py
@@ -13,8 +13,14 @@ def print_menu() -> None:
     print(menu_text)
 
 
+def get_users_choice() -> int:
+    return int(input("\tChoice (1, 2, 3, 4, 5): "))
+
+
 def main():
     print_menu()
+    users_ending = get_users_choice()
+    print(users_ending)
 
 
 if __name__ == "__main__":
