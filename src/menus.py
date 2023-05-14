a35934a9b17e9a1cf1e627566fa2b4a7ecec59aa
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-13 22:53:53 -0500

    Add print_menu function

--- src/menus.py
+++ src/menus.py
@@ -1,9 +1,21 @@
 """menus.py"""
 
 
-def main():
-    ...
+def print_menu() -> None:
+    menu_text = (
+        "\tSelect your game ending:\n"
+        "\t\t1. Draw\n"
+        "\t\t2. White Wins\n"
+        "\t\t3. White wins by checkmate\n"
+        "\t\t4. Black Wins\n"
+        "\t\t5. Black wins by checkmate\n"
+    )
+    print(menu_text)
 
-  
+
+def main():
+    print_menu()
+
+
 if __name__ == "__main__":
     main()
