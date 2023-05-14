8828415900c4f2f6320eaea9dec04f5a9ca52fc4
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-14 14:12:49 -0500

    Add menus.py calls to main.py file

--- src/main.py
+++ src/main.py
@@ -12,3 +12,25 @@ itsf4llofstars
 irooted4hal@mailfence.com
 2023
 """
+import menus as m
+import os
+
+os.system("clear")
+
+m.print_menu()
+user_end_choice = m.get_users_choice()
+
+os.system("clear")
+
+m.min_max_moves()
+
+max_move = m.get_moves()
+
+strip_mate = None
+if user_end_choice == 3 or user_end_choice == 5:
+    strip_mate = m.strip_mate_query()
+
+if strip_mate is not None:
+    print(f"{user_end_choice = } {max_move = } {strip_mate = }")
+else:
+    print(f"{user_end_choice = } {max_move = }")
