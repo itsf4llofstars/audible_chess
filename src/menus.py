c3a196ecf28f7d7d3fabf810889ddbcb9df6a9af
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-13 23:07:14 -0500

    Add min_max_moves print function

--- src/menus.py
+++ src/menus.py
@@ -17,11 +17,28 @@ def get_users_choice() -> int:
     return int(input("\tChoice (1, 2, 3, 4, 5): "))
 
 
+def min_max_moves() -> None:
+    move_str = (
+        "\n\n\tThe minimum move for a game is currently hard set at 20.\n"
+        "\tThe maximum move is left up to you, please enter the maximum\n"
+        "\tnumber of moves you want for your study game.\n\n"
+        "\tExample: Entering 38 can result in games ending between 20 and 38\n"
+        "\tmoves, (inclusive). There is no need to add leading or trailing\n"
+        "\tspaces or, a period."
+    )
+    print(move_str)
+
+
 def main():
     print_menu()
     users_ending = get_users_choice()
     print(users_ending)
+    os.system("clear")
+    min_max_moves()
 
 
 if __name__ == "__main__":
+    import os
+    os.system("clear")
+
     main()
