a463b7c8f44c140cc4699b504352c4f245f55dc1
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-14 22:10:31 -0500

    Add mild error check to get_users_choice

--- src/menus.py
+++ src/menus.py
@@ -16,7 +16,10 @@ def print_menu() -> None:
 
 def get_users_choice() -> int:
     """Error checking required"""
-    return int(input("\tChoice (1, 2, 3, 4, 5): "))
+    choice = int(input("\tChoice (1 - 6): "))
+    if 0 < choice < 7:
+        return choice
+    raise ValueError("Choice must be an number between 1 and 6.")
 
 
 def min_max_moves() -> None:
