ac3830c4076c084e07910c034b68fe8a69fd7252
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-21 01:52:34 -0500

    Remove uneeded get_strip_mate function

--- src/functions.py
+++ src/functions.py
@@ -114,10 +114,6 @@ def set_max_move(max_move):
     regex["max_move"] = f" {max_move}. "
 
 
-def get_strip_mate() -> str:
-    return str(input("\tWould your like to strip that last mate move? [y/n]: "))
-
-
 def main():
     # users_choice = get_users_choice()
     # print(f"{users_choice = }")
