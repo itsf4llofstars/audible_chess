90574da21d0b20acad5636e2b953ccdd1ecce763
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-21 21:35:09 -0500

    Add menu and functions calls to main.py

--- src/main.py
+++ src/main.py
@@ -20,46 +20,19 @@ import sys
 
 os.system("clear")
 
-# m.print_menu()
-# user_end_choice = m.get_users_choice()
-user_end_choice = 2
+print(m.endings_menu())
+user_ending = f.get_users_choice()
 
-os.system("clear")
+print(m.min_max_moves())
+user_max_move = f.get_moves()
+f.set_max_move(user_max_move)
 
-# m.min_max_moves()
+user_mate = f.strip_mate_query()
+user_move_time = f.move_seconds()
 
-# max_move = m.get_moves()
-max_move = 30
-
-strip_mate = None
-if user_end_choice == 3 or user_end_choice == 5:
-    strip_mate = m.strip_mate_query()
-
-# if strip_mate is not None:
-#     print(f"{user_end_choice = } {max_move = } {strip_mate = }")
-# else:
-#     print(f"{user_end_choice = } {max_move = }")
-
-f.set_max_move(max_move)
-
-pgn_file = os.path.expanduser(
-    os.path.join("/", "media", "bumper", "EDD2-E40F", "raspi32", "lichess_short.pgn")
-)
-all_games = f.fast_pgn_file(pgn_file)
-[print(game) for game in all_games]
-
-sys.exit()
-print(m.ending_menu())
-game_ending = f.get_ending()
-
-os.system("clear")
-print(m.strip_mate())
-strip_mate = f.get_strip_mate()
-
-os.system("clear")
-print(m.max_moves())
-max_move = f.get_max_move()
-
-print(m.pgn_path())
-print(m.pgn_file_name())
-print(m.move_seconds())
+print(f"{user_ending = }")
+print(f"{user_max_move = }")
+print(f"{pp.regex['max_move'] = }")
+print(f"{pp.regex['max_move_re'] = }")
+print(f"{user_mate = }")
+print(f"{user_move_time = }")
