670567dd33a36442d9e400ed35c194c0349697b3
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-22 19:35:12 -0500

    Add read file and return a list of string games

--- src/pgn_parsers.py
+++ src/pgn_parsers.py
@@ -1,11 +1,28 @@
 """pgn_parsers.py"""
 
-pattern = {"star": "1. ",}
+pattern = {
+    "start": "1. ",
+}
 
 
-def main():
-    ...
+def read_file(filename: str) -> None:
+    try:
+        with open(filename) as read:
+            file_lines = read.readlines()
+    except FileNotFoundError as fnfe:
+        print(f"{fnfe}")
+    else:
+        index = 0
+        for line in file_lines:
+            if not line.startswith(pattern["start"]):
+                file_lines.pop(index)
+                index -= 1
+            index += 1
+        if file_lines:
+            return file_lines
+        else:
+            raise Exception("File Parser Fail")
 
 
-if __name__ == "__main__":
-    main()
+pgn_file_lines = read_file("/media/bumper/EDD2-E40F/raspi32/lichess_short.pgn")
+[print(line) for line in pgn_file_lines]
