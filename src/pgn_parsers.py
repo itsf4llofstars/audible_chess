5ead2397d55dc1c91320f2cedf1620fe16e90cdf
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-22 21:46:13 -0500

    Add min_max_move function
    Removes lines that don't have move 20 in them and lines
    that have max_move number in them

--- src/pgn_parsers.py
+++ src/pgn_parsers.py
@@ -21,11 +21,23 @@ def read_file(filename: str) -> None:
             return games
 
 
+def min_max_move(games):
+    index = 0
+    while index < len(games):
+        if (
+            not pattern["min_move"] in games[index]
+            or pattern["max_move"] in games[index]
+        ):
+            games.pop(index)
+            index -= 1
+
+        index += 1
+
+
 def main():
-    pgn_file_lines = read_file("/media/bumper/EDD2-E40F/raspi32/lichess_short.pgn")
-    pgn_file_lines = read_file(
-        "/home/bumper/python/audible_chess/docs/test_read_file.pgn"
-    )
+    # chess_games = read_file("/media/bumper/EDD2-E40F/raspi32/lichess_short.pgn")
+    chess_games = read_file("/home/bumper/python/audible_chess/docs/test_read_file.pgn")
+
     [print(line) for line in pgn_file_lines]
 
 
