d2e04397bd7800cb82ec75016d535eb66a67d664
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-23 02:42:48 -0500

    Add no_kibitz() function

--- src/pgn_parsers.py
+++ src/pgn_parsers.py
@@ -5,6 +5,14 @@ pattern = {
     "start": "1. ",
     "min_move": " 20. ",
     "max_move": " 40. ",  # NOTE: Must be removed for final release. Must be 40 for tests
+    "paren_o": "(",
+    "paren_c": ")",
+    "brace_o": "{",
+    "brace_c": "}",
+    "bracket_o": "[",
+    "bracket_c": "]",
+    "tag_o": "<",
+    "tag_c": ">",
 }
 
 
@@ -36,14 +44,33 @@ def min_max_move(games):
     return games
 
 
+def no_kibitz(games):
+    index = 0
+    while index < len(games):
+        if (
+            pattern["paren_o"] in games[index]
+            or pattern["paren_c"] in games[index]
+            or pattern["brace_o"] in games[index]
+            or pattern["brace_c"] in games[index]
+            or pattern["bracket_o"] in games[index]
+            or pattern["bracket_c"] in games[index]
+            or pattern["tag_o"] in games[index]
+            or pattern["tag_c"] in games[index]
+        ):
+            games.pop(index)
+            index -= 1
+
+        index += 1
+    return games
+
+
 def main():
     pgn_file = os.path.expanduser(os.path.join("~", "chess", "chess.pgn"))
 
     chess_games = read_file(pgn_file)
     chess_games = min_max_move(chess_games)
-    print(len(chess_games))
-    print(chess_games[10])
-    # [print(game) for game in chess_games]
+    chess_games = no_kibitz(chess_games)
+    [print(game) for game in chess_games]
 
 
 if __name__ == "__main__":
