9a14883cd53c9de0fb61af5c6ccee3091c3eb8df
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-25 16:42:57 -0500

    Add test_regex_pgn_file method

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,9 +1,19 @@
 """test_functions.py"""
+import os
 import unittest
+
+from src.functions import regex_pgn_file
 
 
 class TestFunctionsFile(unittest.TestCase):
-    pass
+    def test_regex_pgn_file(self):
+        test_pgn = os.path.expanduser(
+            os.path.join("~", "python", "audible_chess", "docs", "test_read_file.pgn")
+        )
+        test_games_list = regex_pgn_file(test_pgn)
+        self.assertEqual(
+            test_games_list, ["1. xx xx 2. xx xx 1-0", "1. xx xx 2. xx xx 0-1"]
+        )
 
 
 if __name__ == "__main__":
