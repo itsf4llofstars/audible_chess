b0038bb3ee148f2e157b5889377a421c5156f052
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-25 23:16:49 -0500

    Write test_scrub_annotations() methode

--- tests/test_pgn_parsers.py
+++ tests/test_pgn_parsers.py
@@ -5,6 +5,7 @@ import unittest
 from src.pgn_parsers import min_max_move
 from src.pgn_parsers import read_file
 from src.pgn_parsers import no_kibitz
+from src.pgn_parsers import scrub_annotations
 
 test_moves = [
     "1. xx xx 19. xx xx 39. xx xx",
@@ -31,6 +32,12 @@ test_kibitz = [
     "1. xx xx 2. < xx xx 3. xx xx",
 ]
 
+test_annotations = [
+    "1. xx xx! 2. xx!! xx? 3. xx?? xx",
+    "1. xx xx?! 2. xx!? xx+ 3. xx xx",
+    "1. xx xx 2. xx xx 3. xx xx",
+]
+
 
 class TestGamesList(unittest.TestCase):
     """Unittests to test the pgn_parser.py file
@@ -42,7 +49,9 @@ class TestGamesList(unittest.TestCase):
             os.path.join("~", "python", "audible_chess", "docs", "test_read_file.pgn")
         )
         test_chess_games = read_file(unittest_pgn)
-        self.assertEqual(test_chess_games, ["1. xx xx 2. xx xx 1-0", "1. xx xx 2. xx xx 0-1"])
+        self.assertEqual(
+            test_chess_games, ["1. xx xx 2. xx xx 1-0", "1. xx xx 2. xx xx 0-1"]
+        )
 
     def test_min_max_move(self):
         test_chess_games = min_max_move(test_moves)
@@ -56,6 +65,17 @@ class TestGamesList(unittest.TestCase):
         self.assertEqual(
             test_chess_games,
             ["1. xx xx 2. xx xx 3. xx xx", "1. xx xx 2. xx xx 3. xx xx"],
+        )
+
+    def test_scrub_annotations(self):
+        test_chess_games = scrub_annotations(test_annotations)
+        self.assertEqual(
+            test_annotations,
+            [
+                "1. xx xx 2. xx xx 3. xx xx",
+                "1. xx xx 2. xx xx 3. xx xx",
+                "1. xx xx 2. xx xx 3. xx xx",
+            ],
         )
 
 
