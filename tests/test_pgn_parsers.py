a08b5c6c817cd5d6d505a35765c8e77f3a0754ae
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-29 22:44:48 -0500

    Write get_black_wins method (ssl)
    Tests get_white_wins/mates and get_black_wins
    fail if running tests together as in: TestGamesList
    But each pass if ran individually

--- tests/test_pgn_parsers.py
+++ tests/test_pgn_parsers.py
@@ -106,6 +106,10 @@ class TestGamesList(unittest.TestCase):
         test_black_mates = get_black_mates(winners_list)
         self.assertEqual(test_black_mates, ["1. xx xx 2. xx xx 3. xx xx# 0-1"])
 
+    def test_get_black_wins(self):
+        test_black_wins = get_black_wins(winners_list)
+        self.assertEqual(test_black_wins, ["1. xx xx 2. xx xx 3. xx xx 0-1"])
+
 
 if __name__ == "__main__":
     unittest.main()
