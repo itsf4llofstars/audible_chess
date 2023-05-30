25cb0807d2774cf90fde8b2d1e22166fdcb971e3
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-29 23:24:41 -0500

    Add parse file initial func w/ try/except

--- src/fast_pgn_parser.py
+++ src/fast_pgn_parser.py
@@ -1,6 +1,20 @@
 """fast_pgn_parsers.py file"""
 
 
+def plarse_pgn_file(filename, min_move: str = " 20. ", max_move: str = " 40. ", white: bool = True, mate: bool False):
+    try:
+        with open(filename) as read:
+            for line in read:
+                line = line.strip()
+    except FileNotFoundError as fnfe:
+        pritn(f"{fnfe}")
+    else:
+        pass
+    finally:
+        pass
+
+
+
 def main():
     pass
 
