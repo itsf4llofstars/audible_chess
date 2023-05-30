e38fc8b22c6016d556ac1a2fd7b122b9a586ce01
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-29 23:29:43 -0500

    Condition to ommit chess games

--- src/fast_pgn_parser.py
+++ src/fast_pgn_parser.py
@@ -1,18 +1,37 @@
 """fast_pgn_parsers.py file"""
 
 
-def plarse_pgn_file(filename, min_move: str = " 20. ", max_move: str = " 40. ", white: bool = True, mate: bool False):
+def plarse_pgn_file(
+    filename,
+    min_move: str = " 20. ",
+    max_move: str = " 40. ",
+    white: bool = True,
+    mate: bool = False,
+):
+    games = []
     try:
         with open(filename) as read:
             for line in read:
                 line = line.strip()
+                if (
+                    not line.startswith("1. ")
+                    or " 40. " in line
+                    or " 20. " not in line
+                    or "(" in line
+                    or "{" in line
+                    or "[" in line
+                    or "<" in line
+                ):
+                    continue
+                else:
+                    games.append(line)
+
     except FileNotFoundError as fnfe:
-        pritn(f"{fnfe}")
+        print(f"{fnfe}")
     else:
         pass
     finally:
         pass
-
 
 
 def main():
