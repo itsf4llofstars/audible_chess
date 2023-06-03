09ebffaebf785fb959f088f463bc9b2963901afb
itsf4llofstars <irooted4hal@mailfence.com>
2023-06-02 21:42:26 -0500

    Add test_parse_pgn_file initial test

--- tests/test_fast_pgn_parser.py
+++ tests/test_fast_pgn_parser.py
@@ -2,8 +2,21 @@
 import os
 import unittest
 
-for src.fast_pgn_parser import parse pgn_file
+from src.fast_pgn_parser import parse_pgn_file
 
 
-if name == "__main__":
+class TestFastParsers(unittest.TestCase):
+    """Unittest  to test the fast_pgn_parser.py
+    file
+    """
+
+    def test_parse_pgn_file(self):
+        unittest_pgn = os.path.expanduser(
+            os.path.join("~", "python", "audible_chess", "docs", "test_read_file.pgn")
+        )
+        test_chess_games = parse_pgn_file(unittest_pgn)
+        self.assertEqual(test_chess_games, ["1. "], ["1. "])
+
+
+if __name__ == "__main__":
     unittest.main()
