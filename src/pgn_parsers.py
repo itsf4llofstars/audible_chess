a924f1b55efc6c351e61774b883e6861f7a6736f
itsf4llofstars <irooted4hal@mailfence.com>
2023-06-29 00:23:41 -0500

    Reaname games to var_games in if name main

--- src/pgn_parsers.py
+++ src/pgn_parsers.py
@@ -2,21 +2,21 @@
 import os
 
 pattern = {
-    "start": "1. ",
-    "min_move": " 20. ",
-    "max_move": " 40. ",  # NOTE: Must be removed for final release. Must be 40 for tests
-    "paren_o": "(",
-    "paren_c": ")",
-    "brace_o": "{",
-    "brace_c": "}",
-    "bracket_o": "[",
-    "bracket_c": "]",
-    "tag_o": "<",
-    "tag_c": ">",
-    "white_wins": " 1-0",
-    "black_wins": " 0-1",
-    "hash": "#",
-}
+        "start": "1. ",
+        "min_move": " 20. ",
+        "max_move": " 40. ",  # NOTE: Must be removed for final release. Must be 40 for tests
+        "paren_o": "(",
+        "paren_c": ")",
+        "brace_o": "{",
+        "brace_c": "}",
+        "bracket_o": "[",
+        "bracket_c": "]",
+        "tag_o": "<",
+        "tag_c": ">",
+        "white_wins": " 1-0",
+        "black_wins": " 0-1",
+        "hash": "#",
+        }
 
 
 def read_file(filename: str):
@@ -37,9 +37,9 @@ def min_max_move(games):
     index = 0
     while index < len(games):
         if (
-            not pattern["min_move"] in games[index]
-            or pattern["max_move"] in games[index]
-        ):
+                not pattern["min_move"] in games[index]
+                or pattern["max_move"] in games[index]
+                ):
             games.pop(index)
             index -= 1
 
@@ -52,15 +52,15 @@ def no_kibitz(games):
     index = 0
     while index < len(games):
         if (
-            pattern["paren_o"] in games[index]
-            or pattern["paren_c"] in games[index]
-            or pattern["brace_o"] in games[index]
-            or pattern["brace_c"] in games[index]
-            or pattern["bracket_o"] in games[index]
-            or pattern["bracket_c"] in games[index]
-            or pattern["tag_o"] in games[index]
-            or pattern["tag_c"] in games[index]
-        ):
+                pattern["paren_o"] in games[index]
+                or pattern["paren_c"] in games[index]
+                or pattern["brace_o"] in games[index]
+                or pattern["brace_c"] in games[index]
+                or pattern["bracket_o"] in games[index]
+                or pattern["bracket_c"] in games[index]
+                or pattern["tag_o"] in games[index]
+                or pattern["tag_c"] in games[index]
+                ):
             games.pop(index)
             index -= 1
 
@@ -145,35 +145,35 @@ def get_black_wins(games):
 
 
 if __name__ == "__main__":
-    games = [
-        "1. xx xx 2. xx xx 3. xx xx",
-        "1. xx xx 2. xx xx 3. xx xx# 0-1",
-        "1. xx xx 2. xx xx 3. xx xx# 0-1",
-        "1. xx xx 2. xx xx 3. xx xx 1-0",
-        "1. xx xx 2. xx xx 3. xx xx# 1-0",
-        "1. xx xx 2. xx xx 3. xx xx 0-1",
-        "1. xx xx 2. xx xx 3. xx xx 1/2-1/2",
-        "1. xx xx 2. xx xx 3. xx xx 1-0",
-        "1. xx xx 2. xx xx 3. xx xx",
-        "1. xx xx 2. xx xx 3. xx xx# 0-1",
-        "1. xx xx 2. xx xx 3. xx xx# 1-0",
-        "1. xx xx 2. xx xx 3. xx xx 1-0",
-        "1. xx xx 2. xx xx 3. xx xx 1/2-1/2",
-    ]
+    var_games = [
+            "1. xx xx 2. xx xx 3. xx xx",
+            "1. xx xx 2. xx xx 3. xx xx# 0-1",
+            "1. xx xx 2. xx xx 3. xx xx# 0-1",
+            "1. xx xx 2. xx xx 3. xx xx 1-0",
+            "1. xx xx 2. xx xx 3. xx xx# 1-0",
+            "1. xx xx 2. xx xx 3. xx xx 0-1",
+            "1. xx xx 2. xx xx 3. xx xx 1/2-1/2",
+            "1. xx xx 2. xx xx 3. xx xx 1-0",
+            "1. xx xx 2. xx xx 3. xx xx",
+            "1. xx xx 2. xx xx 3. xx xx# 0-1",
+            "1. xx xx 2. xx xx 3. xx xx# 1-0",
+            "1. xx xx 2. xx xx 3. xx xx 1-0",
+            "1. xx xx 2. xx xx 3. xx xx 1/2-1/2",
+            ]
 
-    index = 0
-    while index < len(games):
-        if pattern["hash"] in games[index]:
-            games.pop(index)
-            index -= 1
-        elif games[index].endswith(pattern["black_wins"]):
-            games.pop(index)
-            index -= 1
-        elif not games[index].endswith(pattern["white_wins"]):
-            games.pop(index)
-            index -= 1
-        index += 1
+    INDEX = 0
+    while INDEX < len(var_games):
+        if pattern["hash"] in var_games[INDEX]:
+            var_games.pop(INDEX)
+            INDEX -= 1
+        elif var_games[INDEX].endswith(pattern["black_wins"]):
+            var_games.pop(INDEX)
+            INDEX -= 1
+        elif not var_games[INDEX].endswith(pattern["white_wins"]):
+            var_games.pop(INDEX)
+            INDEX -= 1
+        INDEX += 1
         os.system("clear")
-        [print(game) for game in games]
-        # print(f"{index = } {len(games)} {games[index]}")
-        input()
+
+        for game in var_games:
+            print(game)
