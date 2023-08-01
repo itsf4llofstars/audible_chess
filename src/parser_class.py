03f562eb6c6119e8eeb7ca855937e807cef8b163
itsf4llofstars <irooted4hal@mailfence.com>
2023-08-01 01:13:11 -0500

    Add patterns dict and front half of fileread

--- src/parser_class.py
+++ src/parser_class.py
@@ -1,9 +1,16 @@
 """parser_class.py"""
+imoprt re
 
 
 class Parser:
     def __init__(self, filename: str):
         self._filename = filename
+        self.raw_games = []
+        self.patterns = {
+            "start": "1. ",
+            "star_re": r"1\.\s",
+            "legal_re": r"^1\.\s[a-hN][3-4acfg]3?\s[a-hN][5-6acfg]6?\s2\.\s",
+        }
 
     @property
     def filename(self):
@@ -14,6 +21,11 @@ class Parser:
         if not isinstance(filename, str):
             raise ValueError("Filename is not a string")
         self._filename = filename
+
+    def read_file(self):
+        try:
+            with open(self.filename, encoding="utf-8") as read:
+                for line in read:
 
 
 def main():
