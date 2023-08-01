4ede3aa93776f6cb1efd81c6d43e281df087711f
itsf4llofstars <irooted4hal@mailfence.com>
2023-08-01 01:05:50 -0500

    Add parser class filename getter setter

--- src/parser_class.py
+++ src/parser_class.py
@@ -1,6 +1,21 @@
 """parser_class.py"""
 
 
+class Parser:
+    def __init__(self, filename: str):
+        self._filename = filename
+
+    @property
+    def filename(self):
+        return self._filename
+
+    @filename.setter
+    def filename(self, filename):
+        if not isinstance(filename, str):
+            raise ValueError("Filename is not a string")
+        self._filename = filename
+
+
 def main():
     ...
 
