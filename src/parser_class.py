14db8c8a504daa32e2c673d7a67e996f21b88d9e
itsf4llofstars <irooted4hal@mailfence.com>
2023-08-01 01:29:13 -0500

    Add patterns (dict not complete)

--- src/parser_class.py
+++ src/parser_class.py
@@ -1,32 +1,36 @@
 """parser_class.py"""
-imoprt re
+import re
 
 
 class Parser:
     def __init__(self, filename: str):
-        self._filename = filename
+        self.filename = filename
         self.raw_games = []
         self.patterns = {
             "start": "1. ",
             "star_re": re.compile(r"1\.\s"),
             "legal_re": re.compile(r"^1\.\s[a-hN][3-4acfg]3?\s[a-hN][5-6acfg]6?\s2\.\s"),
+            "to_long": " 40. ",
+            "to_short": " 20. ",  # should be changed for production release
+            "paren_o": "(",
+            "paren_c": ")",
+            "brace_o": "{",
+            "brace_c": "}",
+            "bracket_o": "[",
+            "bracket_c": "]",
+            "tag_o": "<",
+            "tag_c": ">",
+            "white": " 1-0",
+            "black": " 0-1",
+            "draw": " 1/2-1/2",
+            "mate": "#",
         }
-
-    @property
-    def filename(self):
-        return self._filename
-
-    @filename.setter
-    def filename(self, filename):
-        if not isinstance(filename, str):
-            raise ValueError("Filename is not a string")
-        self._filename = filename
 
     def read_file(self):
         try:
             with open(self.filename, encoding="utf-8") as read:
                 [self.raw_games.append(line.rstrip()) for line in read
-                 if not line.startswith(patterns["start"])]
+                 if not line.startswith(self.patterns["start"])]
         except FileNotFoundError as fnfe:
             raise FileNotFoundError("File not found") from fnfe
 
